2a403dcd2020dd132fb1488c554cc9365713a2a2
Ayush <mhetreayush1719@gmail.com>
2023-06-18 00:46:08 +0530

    ⚠⚠ Add offline alert

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,17 @@
 import LoginWrapper from "@/components/LoginWrapper";
-import PageWrapper from "@/components/Pagewrapper";
 import "@/styles/globals.css";
-import { useEffect } from "react";
+import { Offline } from "react-detect-offline";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    window.onoffline = () => {
-      alert("You are offline");
-    };
-  }, []);
   return (
     <>
       <LoginWrapper>
         <Component {...pageProps} />
+        <Offline>
+          <div className="bg-black text-white absolute p-4 top-[25%] left-[25%]">
+            <p>You are offline. Please check your internet connection.</p>
+          </div>
+        </Offline>
       </LoginWrapper>
     </>
   );
